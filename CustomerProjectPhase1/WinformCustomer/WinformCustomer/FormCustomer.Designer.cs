75fa277002b99310662a52fb366b69f26d39ed53
razaman2 <razamvmt@gmail.com>
2017-07-08 09:08:36 -0400

    added user interface to solution

---
+++ CustomerProjectPhase1/WinformCustomer/WinformCustomer/FormCustomer.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace WinformCustomer
+{
+    partial class FormCustomer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataOutput = new System.Windows.Forms.DataGridView();
+            this.txtCustomerName = new System.Windows.Forms.TextBox();
+            this.txtPhoneNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnValidate = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtBillAmount = new System.Windows.Forms.TextBox();
+            this.txtBillDate = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.RichTextBox();
+            this.cmbCustomerType = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataOutput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataOutput
+            // 
+            this.dataOutput.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataOutput.Location = new System.Drawing.Point(12, 175);
+            this.dataOutput.Name = "dataOutput";
+            this.dataOutput.Size = new System.Drawing.Size(748, 251);
+            this.dataOutput.TabIndex = 1;
+            // 
+            // txtCustomerName
+            // 
+            this.txtCustomerName.Location = new System.Drawing.Point(122, 47);
+            this.txtCustomerName.Name = "txtCustomerName";
+            this.txtCustomerName.Size = new System.Drawing.Size(159, 20);
+            this.txtCustomerName.TabIndex = 2;
+            this.txtCustomerName.Leave += new System.EventHandler(this.txtCustomerName_Leave);
+            // 
+            // txtPhoneNumber
+            // 
+            this.txtPhoneNumber.Location = new System.Drawing.Point(122, 86);
+            this.txtPhoneNumber.Name = "txtPhoneNumber";
+            this.txtPhoneNumber.Size = new System.Drawing.Size(159, 20);
+            this.txtPhoneNumber.TabIndex = 3;
+            this.txtPhoneNumber.Leave += new System.EventHandler(this.txtPhoneNumber_Leave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Customer Type";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Customer Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Phone Number";
+            // 
+            // btnValidate
+            // 
+            this.btnValidate.Location = new System.Drawing.Point(15, 128);
+            this.btnValidate.Name = "btnValidate";
+            this.btnValidate.Size = new System.Drawing.Size(75, 23);
+            this.btnValidate.TabIndex = 7;
+            this.btnValidate.Text = "Validate";
+            this.btnValidate.UseVisualStyleBackColor = true;
+            this.btnValidate.Click += new System.EventHandler(this.btnValidate_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(440, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Bill Amount";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(440, 50);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Bill Date";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(440, 89);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Address";
+            // 
+            // txtBillAmount
+            // 
+            this.txtBillAmount.Location = new System.Drawing.Point(526, 12);
+            this.txtBillAmount.Name = "txtBillAmount";
+            this.txtBillAmount.Size = new System.Drawing.Size(159, 20);
+            this.txtBillAmount.TabIndex = 11;
+            this.txtBillAmount.Leave += new System.EventHandler(this.txtBillAmount_Leave);
+            // 
+            // txtBillDate
+            // 
+            this.txtBillDate.Location = new System.Drawing.Point(526, 47);
+            this.txtBillDate.Name = "txtBillDate";
+            this.txtBillDate.Size = new System.Drawing.Size(159, 20);
+            this.txtBillDate.TabIndex = 12;
+            this.txtBillDate.Leave += new System.EventHandler(this.txtBillDate_Leave);
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(526, 89);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(234, 62);
+            this.txtAddress.TabIndex = 15;
+            this.txtAddress.Text = "";
+            this.txtAddress.Leave += new System.EventHandler(this.txtAddress_Leave);
+            // 
+            // cmbCustomerType
+            // 
+            this.cmbCustomerType.FormattingEnabled = true;
+            this.cmbCustomerType.Items.AddRange(new object[] {
+            "Customer",
+            "Lead"});
+            this.cmbCustomerType.Location = new System.Drawing.Point(122, 15);
+            this.cmbCustomerType.Name = "cmbCustomerType";
+            this.cmbCustomerType.Size = new System.Drawing.Size(159, 21);
+            this.cmbCustomerType.TabIndex = 16;
+            this.cmbCustomerType.SelectedIndexChanged += new System.EventHandler(this.cmbCustomerType_SelectedIndexChanged);
+            // 
+            // FormCustomer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(772, 438);
+            this.Controls.Add(this.cmbCustomerType);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtBillDate);
+            this.Controls.Add(this.txtBillAmount);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnValidate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPhoneNumber);
+            this.Controls.Add(this.txtCustomerName);
+            this.Controls.Add(this.dataOutput);
+            this.Name = "FormCustomer";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataOutput)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataOutput;
+        private System.Windows.Forms.TextBox txtCustomerName;
+        private System.Windows.Forms.TextBox txtPhoneNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnValidate;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtBillAmount;
+        private System.Windows.Forms.TextBox txtBillDate;
+        private System.Windows.Forms.RichTextBox txtAddress;
+        private System.Windows.Forms.ComboBox cmbCustomerType;
+    }
+}
+
---
+++ CustomerProjectPhase1/WinformCustomer/WinformCustomer/FormCustomer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MiddleLayer;
+using FactoryCustomer;
+
+namespace WinformCustomer
+{
+    public partial class FormCustomer : Form
+    {
+        private CustomerBase cust = null;
+
+        public FormCustomer()
+        {
+            InitializeComponent();
+            cmbCustomerType.Text = "Customer";
+            txtBillAmount.Text = Convert.ToString(0);
+            txtBillDate.Text = Convert.ToString(DateTime.Today);
+        }
+
+        // Runs Validate Function for Customer Type
+        private void btnValidate_Click(object sender, EventArgs e)
+        {
+            cust.Validate();
+        }
+
+        // Assigns Type of Customer on change
+        private void cmbCustomerType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cust = Factory.Create(cmbCustomerType.Text);
+        }
+
+        // Assign value of textbox to CustomerName property
+        private void txtCustomerName_Leave(object sender, EventArgs e)
+        {
+            cust.CustomerName = txtCustomerName.Text;
+        }
+
+        // Assign value of textbox to PhoneNumber property
+        private void txtPhoneNumber_Leave(object sender, EventArgs e)
+        {
+            cust.PhoneNumber = txtPhoneNumber.Text;
+        }
+
+        // Assign value of textbox to BillAmount property
+        private void txtBillAmount_Leave(object sender, EventArgs e)
+        {
+            cust.BillAmount = Convert.ToDecimal(txtBillAmount.Text);
+        }
+
+        // Assign value of textbox to BillDate property
+        private void txtBillDate_Leave(object sender, EventArgs e)
+        {
+            cust.BillDate = Convert.ToDateTime(txtBillDate.Text);
+        }
+
+        // Assign value of textbox to Address property
+        private void txtAddress_Leave(object sender, EventArgs e)
+        {
+            cust.Address = txtAddress.Text;
+        }
+    }
+}
